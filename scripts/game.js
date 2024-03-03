6917924f4bdece331f7a956bc67b2fd22754db83
H-M-1999 <81930506@students.liu.edu.lb>
2024-03-03 20:20:39 +0200

    finished the hanging logic

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,10 @@
-const hangs = document.getElementById("hang");
 const answerSection = document.getElementById("answer-section");
 const letter = document.querySelectorAll(".letter");
 
 
 const words =['CAT','DOG','ELEPHANT','GIRAFFE','HIPPO','KANGAROO','LION','MONKEY','PENGUIN','RHINO','TIGER','ZEBRA'];
+let mistakes = 0;
+
 
 function randomWord(){
     let random = Math.floor(Math.random()*words.length);
@@ -27,18 +28,57 @@ displayWord(random);
 
 function correctLetter(word , letter){
     word = random;
+    let a = 0;
     for(let i=0; i<word.length; i++)
     {
         if(letter.innerHTML === word[i])
         {
             let span = document.getElementById(i);
             span.innerHTML = letter.innerHTML;
+            a++;
         }
     }
+    if(a>0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function startHanging(){
+
 }
 
 letter.forEach((letter)=>{  
     letter.addEventListener("click", function(){
+        if(correctLetter(random, letter))
         correctLetter(random, letter);
-    })
+    
+    else{
+        mistakes++;
+        if(mistakes === 1){
+            head();
+        }
+        else if(mistakes === 2){
+            body();
+        }
+        else if(mistakes === 3){
+            leftHand();
+        }
+        else if(mistakes === 4){
+            rightHand();
+        }
+        else if(mistakes === 5){
+            leftLeg();
+        }
+        else if(mistakes === 6){
+            rightLeg();
+            alert("Game Over the word was " + random + "!");
+            window.location.reload();
+        }
+    }
+    return mistakes;
 })
+})
+
